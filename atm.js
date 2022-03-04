830b0027709547135d3480e1c2d4acc60c862e77
Garett Bare <Garett.Bare@yahoo.com>
2022-03-04 17:26:48 -0600

    added prompt-sync and initializing exporting of functions

--- atm.js
+++ atm.js
@@ -1,20 +1,22 @@
 "use strict";
 
 function getBalance(){
-
+    return('hello GB')
 }
 
 function withdraw(){
-
+    console.log("Hello WD")
 }
 
 function deposit() {
-
+    print("Hello Depo")
 
 }
 
 function validatePin(){
-
+    print("Hello validate")
 
 }
 
+
+module.exports = {getBalance, withdraw, deposit, validatePin}
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 "use strict";
+
+const ATM = require('./atm.js')
+
+console.log(ATM.getBalance())
